c4e029b7ee0f9402374b09c864d26d54305cec32
Jim Ries <RiesJ@missouri.edu>
2021-03-22 15:13:42 -0500

    Initial roll-in

---
+++ lab11.c
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ * CS1050
+ * SP2021
+ * Lab 11
+ * By Jim Ries (jer676)
+ * March 22, 2021
+ *****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "fight.h"
+int main(int argc, char * argv[])
+{
+    // Uncomment the following line if you want to try a sample fight
+    //Fight("JimR",21,900,10,"5d6+5","5d12+5","N/A","Lar",5,100,5,"1d6","N/A","N/A");
+}
